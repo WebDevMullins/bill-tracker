d38734defa536e63c41a2162c4c54902cfad8be9
Brandon Mullins <webdevmullins@gmail.com>
2024-09-29 18:50:58 -0500

    add Button and update app folder structure

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+	return (
+		<div className='container py-12'>
+			<h1 className='text-4xl'>Bill Tracker</h1>
+			<Button>Click me</Button>
+		</div>
+	)
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<main>
-			<h1>Bill Tracker</h1>
-		</main>
-	)
-}
