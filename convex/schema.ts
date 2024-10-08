eab2451e606e472af4231630c6162815e10658f1
Brandon Mullins <webdevmullins@gmail.com>
2024-10-07 19:31:28 -0500

    add payees to schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,12 +2,18 @@ import { defineSchema, defineTable } from 'convex/server'
 import { v } from 'convex/values'
 
 export default defineSchema({
-	// Other tables here...
-
+	// bills table schema
 	bills: defineTable({
 		amount: v.float64(),
 		dueDate: v.string(),
 		name: v.string(),
-		isPaid: v.boolean()
-	}).index('by_dueDate', ['dueDate'])
+		isPaid: v.boolean(),
+		payeeId: v.id('payees') // foreign key to payees table
+	}).index('by_dueDate', ['dueDate']),
+
+	// payees table schema
+	payees: defineTable({
+		name: v.string(),
+		website: v.string()
+	}).index('by_name', ['name'])
 })
